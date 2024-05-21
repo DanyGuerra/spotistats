76e8bdd20114daeea032bb3a7226b622fb2f3732
DanyGuerra <luis_daniel.grra15@hotmail.com>
2024-05-21 15:28:49 -0600

    feat: :sparkles: Adding dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { HelpController } from './help/help.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [HelpController],
   providers: [],
   exports: [],
