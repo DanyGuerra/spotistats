994007f332c86399a5a983fa9b64069821cd1c3f
DanyGuerra <luis_daniel.grra15@hotmail.com>
2024-05-26 21:50:47 -0600

    feat: Login redirect auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,32 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthLogDto } from 'src/auth/create-auth-log.dto';
+import { Response } from 'express';
+import * as queryString from 'querystring';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @Get('login')
+  login(@Res() res: Response) {
+    const hostApiSpotify = this.configService.get<string>(
+      'hostAccountsApiSpotify',
+    );
+
+    // TODO: agregar todas las query params
+    const queryParams = {
+      response_type: 'code',
+    };
+
+    res.redirect(
+      `${hostApiSpotify}/authorize?${queryString.stringify(queryParams)}`,
+    );
+  }
 
   @Get('callback')
   async authCallBack(@Query() authLogs: CreateAuthLogDto) {
