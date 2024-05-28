b0a0c28c63c15651e625c2fb9493879b95a42e00
DanyGuerra <luis_daniel.grra15@hotmail.com>
2024-05-28 17:19:41 -0600

    feat: AutlogsSchema with timestamp

--- src/auth/auth-logs.schema.ts
+++ src/auth/auth-logs.schema.ts
@@ -1,9 +1,25 @@
-import { Schema } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
 
-export const AuthLogSchema = new Schema({
-  code: String,
-  state: String,
-  access_token: String,
-  refresh_token: String,
-  usernameId: String,
-});
+@Schema({ timestamps: true })
+export class AuthLog extends Document {
+  @Prop()
+  code?: string;
+
+  @Prop()
+  access_token?: string;
+
+  @Prop()
+  refresh_token?: string;
+
+  @Prop()
+  usernameId?: string;
+
+  @Prop({ default: Date.now })
+  createdAt?: Date;
+
+  @Prop({ default: Date.now })
+  updatedAt: Date;
+}
+
+export const AuthLogSchema = SchemaFactory.createForClass(AuthLog);
