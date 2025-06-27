a433cc95144aa0be36a4fc48ee16002d40f7cd0d
DanyGuerra <luis_daniel.grra15@hotmail.com>
2025-06-27 17:28:48 -0600

    refactor: :recycle: Revert delete auth old log

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -89,12 +89,6 @@ export class AuthController {
         token.access_token,
       );
 
-      const oldLog = await this.authService.getAuthLogByUserId(usernameId.id);
-
-      if (oldLog) {
-        await this.authService.deleteAuthLog(oldLog.id);
-      }
-
       const dataUpdate: CreateAuthLogDto = {
         accessToken: token.access_token,
         refreshToken: token.refresh_token,
